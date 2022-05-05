823141709a25ab8c9797eceb3c9a57924360cef6
Sam Bauch <sbauch@gmail.com>
2022-05-05 10:55:46 -0400

    deploy script using package

--- deploy/001_deploy_counter.ts
+++ deploy/001_deploy_counter.ts
@@ -1,28 +1,17 @@
 import {HardhatRuntimeEnvironment} from 'hardhat/types';
 import {DeployFunction} from 'hardhat-deploy/types';
 import {getChainId} from 'hardhat';
+import {EssentialForwarderDeployments} from '@0xessential/signers';
 
 const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   const {deployer} = await hre.getNamedAccounts();
   const {deploy} = hre.deployments;
 
-  const networkName = await getChainId().then(
-    (id) =>
-      ({
-        80001: 'mumbai',
-        137: 'matic',
-      }[id])
-  );
-
-  if (!networkName) return;
-
-  // eslint-disable-next-line @typescript-eslint/no-var-requires
-  const ForwarderDeployment = require(`../deployments/${networkName}/EssentialForwarder.json`);
+  const chain = (await getChainId()) as keyof typeof EssentialForwarderDeployments;
 
   await deploy('Counter', {
     from: deployer,
-
-    args: [ForwarderDeployment.address],
+    args: [EssentialForwarderDeployments[chain].address],
     log: true,
   });
 };
